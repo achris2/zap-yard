b375093aac13b946528d84cfe99a63deb9c25d81
Alex Christakou <alexchristakou@gmail.com>
2024-03-28 12:40:30 +0000

    built out nav bar, added logo

---
+++ zap-yard/components/navbar.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
+import DesktopLogo from '../public/zapyard-desktoplogo.png';
+import MobileLogo from '../public/zapyard-symbol.png';
+import UserNav from './usernav';
+
+export default function Navbar() {
+  return (
+      <nav className="w-full border-b">
+          <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+              <Link href="/">
+                  <Image
+                      src={DesktopLogo}
+                      alt="Zap Yard Logo"
+                      className="w-8 hidden lg:block"
+                  />
+                  
+                  <Image
+                        src={MobileLogo}
+                        alt="Mobile Zap Yard business logo"
+                        className="block lg:hidden w-20"
+                  /> 
+              </Link>
+              <div className="rounded-full border px-5 py-2">
+                  <h1>Search Bar</h1>
+              </div>
+              <UserNav></UserNav>
+          </div>
+      </nav>
+  )
+}
---
+++ zap-yard/components/usernav.tsx
@@ -0,0 +1,21 @@
+import { MenuIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import AnonUserImg from '../public/anonuser.webp';
+import React from 'react';
+import Image from "next/image";
+
+export default function UserNav() {
+  return (
+      <DropdownMenu>
+          <DropdownMenuTrigger>
+              <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+                  <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" /> 
+                  <Image src={AnonUserImg}
+                      alt="User Avatar"
+                      className="rounded-full h-8 w-8 hidden lg:block"
+                  />
+              </div>
+          </DropdownMenuTrigger>
+    </DropdownMenu>
+  )
+}
